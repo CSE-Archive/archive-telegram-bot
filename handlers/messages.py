47f032df13e47d36a775c0c33fbbd0be62343ab8
Reza Farjam <reza.farjam78@gmail.com>
2022-06-22 23:08:22 +0430

    Move environment variables to main file

--- handlers/messages.py
+++ handlers/messages.py
@@ -1,5 +1,3 @@
-import os
-
 from telegram import Update
 from telegram.ext import ContextTypes
 from telegram.helpers import escape_markdown
@@ -8,17 +6,19 @@ from telegram.constants import ParseMode
 
 async def forward(update: Update, context: ContextTypes) -> None:
     user = update.effective_user
+    group_id = context.bot_data.get("GROUP_ID")
+
     sender_info = f"_id:_ {user.id}\n" \
                   f"_name:_ {escape_markdown(user.full_name, version=2)}\n" \
                   f"_username:_ {escape_markdown('@' + user.username if user.username else '-', version=2)}"
 
     try:
         forwarded_message = await update.effective_message.forward(
-            chat_id=os.getenv("GROUP_ID"),
+            chat_id=group_id,
         )
 
         await context.bot.send_message(
-            chat_id=os.getenv("GROUP_ID"),
+            chat_id=group_id,
             text=sender_info,
             reply_to_message_id=forwarded_message.message_id,
             parse_mode = ParseMode.MARKDOWN_V2,
@@ -33,7 +33,7 @@ async def forward(update: Update, context: ContextTypes) -> None:
     except Exception as e:
         print(e)
 
-        await update.effective_message.reply_html(
-            text=f"_✗ ارسال نشد. متن خطا:_\n{escape_markdown(str(e), version=2)}",
+        await update.effective_message.reply_markdown_v2(
+            text=f"_✗ ارسال نشد\. متن خطا:_\n{escape_markdown(str(e), version=2)}",
             quote=True,
         )
--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from handlers.messages import forward
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
 
+TOKEN = os.getenv("TOKEN")
+GROUP_ID = os.getenv("GROUP_ID")
+
+
 # Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -15,10 +19,11 @@ logger = logging.getLogger(__name__)
 
 
 def main() -> None:
-    app = ApplicationBuilder().token(os.getenv("TOKEN")).build()
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.bot_data = {"GROUP_ID": GROUP_ID}
 
     app.add_handler(CommandHandler("start", start))
-
     app.add_handler(
         MessageHandler(
             filters.ChatType.PRIVATE & ~(filters.COMMAND),
